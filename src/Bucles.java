51b41c18bb94cfdea2b805faf5d5c0d39288290b
Adrian <amosqueraalvarez@danielcastelao.org>
2026-04-07 13:13:32 +0200

    inicios java

---
+++ src/Bucles.java
@@ -0,0 +1,53 @@
+// bucles en java
+import java.util.Scanner;
+
+public class Bucles {
+    public static void main (String[] args){
+        int contador=0;
+        while (contador <= 5){
+            System.out.println(contador);
+            contador++;
+        }
+        // scanner se utiliza como el input en python
+        Scanner teclado =new Scanner (System.in);
+        int opcion=0;
+        while (opcion != 3) {
+            System.out.println("Elixa unha opcion");
+            System.out.println("1.Saudar");
+            System.out.println("2.Despedirse");
+            System.out.println("3.Sair");
+            // este metodo recoge la respuesta del input
+            String op = teclado.nextLine();
+            // asi se convierte un string a un integer en vez de esto puedo usar antes el metodo .nextInt
+            opcion =  Integer.parseInt(op);
+            switch(opcion) {
+                case 1:
+                    System.out.println("Bienvenido");
+                    break;
+                case 2:
+                    System.out.println("Hasta luego");
+                    break;
+                case 3:
+                    System.out.println("Programa cerrado");
+                    break;
+                default:
+                    System.out.println("Error de opcion");
+                    break;
+            }
+        }
+        //bucle for
+
+        //for(inicializacion ; expresion condicional; incremento ){}
+        for (int i=5; i<20 ;i+=3){
+            System.out.println("Indice: " + i);
+        }
+        // bucle infinito con codicion de salida y break
+        int i=5;
+                for(;;){
+                    System.out.println("Indice: "+i);
+                    i+=3;
+                    if (i>19) break;
+                }
+    }
+
+}
---
+++ src/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    public static void main (String [] args){
+        int numero= 512;
+        byte numPequeno=(byte) numero;
+        System.out.println(numPequeno);
+    }
+}
