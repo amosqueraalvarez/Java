5790fbd421f30ac7e51011538333b3b08652bcf6
Adrian <amosqueraalvarez@danielcastelao.org>
2026-04-08 13:54:03 +0200

    POO

---
+++ src/Persoa.java
@@ -0,0 +1,20 @@
+// El nombre de la clase y del fichero tienen que ser el mismo
+// Para poder acceder desde fuera a la clase tiene que ser public
+// Puede haber una clase privada siempre que no sea la principal del fichero
+// Las clases pueden ser public, private o protected que no se escribe nada igual que en python
+// Public, cualquiera puede acceder, Private no se puede acceder, Protected solo se puede acceder si
+// esta dentro de la misma carpeta
+
+
+public class Persoa {
+    //declaracion de propiedades primero
+    public String nome;
+    public String dni;
+    public int edade;
+    // constructor de la clase
+    public Persoa (String nome, String dni, int edade){
+
+
+    }
+
+}
